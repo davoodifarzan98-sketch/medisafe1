ae3563cc0eef157bf73a0e4ad792184f3c940608
davoodifarzan98-sketch <davoodi.farzan98@gmail.com>
2025-11-14 16:57:56 +0100

    Initial Commit

---
+++ medisafe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  medisafe
+//
+//  Created by farzan on 14/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ medisafe/medisafeApp.swift
@@ -0,0 +1,17 @@
+//
+//  medisafeApp.swift
+//  medisafe
+//
+//  Created by farzan on 14/11/25.
+//
+
+import SwiftUI
+
+@main
+struct medisafeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
